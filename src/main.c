5abef7c2b73a4b715d2ea5b9c9e11a24f35e40b0
reallocmb <realloc.mb@gmail.com>
2024-03-27 23:58:05 +0100

    u

--- src/main.c
+++ src/main.c
@@ -4,35 +4,102 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<fcntl.h>
-#include<stdint.h>
 #include<errno.h>
 #include<string.h>
 #include<unistd.h>
 #include<sys/stat.h>
 
 typedef uint32_t u32;
+typedef int32_t s32;
 
 u32 shader_load(const char *vertex_path, const char *fragment_path)
 {
-    u32 fd = open(vertex_path, O_RDONLY, S_IRUSR);
+    s32 success;
+    char info_log[512];
+    u32 shader_program;
+    u32 shader_vertex;
+    u32 shader_fragment;
+    s32 fd;
+    fd = open(vertex_path, O_RDONLY);
     if (fd == -1)
     {
-        fprintf(stderr, "F: %s L: %d -> errno: %s\n",
+        fprintf(stderr,
+                "F: %s L: %d -> errno: %s\n",
                 __FILE__, __LINE__, strerror(errno));
     }
-    char *buffer;
+    char *buffer = NULL;
     struct stat statbuf;
 
     fstat(fd, &statbuf);
-    buffer = realloc(buffer, sizeof(*buffer) * statbuf.st_size);
+    printf("size: %ld\n", statbuf.st_size);
+    buffer = realloc(buffer, sizeof(*buffer) * statbuf.st_size + 1);
 
     read(fd, buffer, statbuf.st_size);
+    buffer[statbuf.st_size] = 0;
 
+    shader_vertex = glCreateShader(GL_VERTEX_SHADER); 
+    glCompileShader(shader_vertex);
+
+    glGetShaderiv(shader_vertex, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader_vertex, 512, NULL, info_log);
+        fprintf(stderr,
+                "ERROR::SHADER::VERTEX::COMPILATION_FAILED -> %s\n",
+                info_log);
+    }
 
     close(fd);
+
+    fd = open(fragment_path, O_RDONLY);
+    if (fd == -1)
+    {
+        fprintf(stderr,
+                "F: %s L: %d -> errno: %s\n",
+                __FILE__, __LINE__, strerror(errno));
+    }
+
+    fstat(fd, &statbuf);
+    buffer = realloc(buffer, sizeof(*buffer) * statbuf.st_size + 1);
+
+    read(fd, buffer, statbuf.st_size);
+    buffer[statbuf.st_size] = 0;
+
+    shader_fragment = glCreateShader(GL_FRAGMENT_SHADER); 
+    glCompileShader(shader_fragment);
+
+    glGetShaderiv(shader_fragment, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader_fragment, 512, NULL, info_log);
+        fprintf(stderr,
+                "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED -> %s\n",
+                info_log);
+    }
+
+    close(fd);
+
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, shader_vertex);
+    glAttachShader(shader_program, shader_fragment);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        fprintf(stderr,
+                "ERROR::SHADER::PROGRAM::COMPILATION_FAILED -> %s\n",
+                info_log);
+    }
+
+
+    glDeleteShader(shader_vertex);
+    glDeleteShader(shader_fragment);
+
+    return shader_program;
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -78,5 +145,6 @@ int main(int argc, char **argv)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     return 0;
 }
